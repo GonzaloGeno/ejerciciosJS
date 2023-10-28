c7564f7188a74b2a18eb5dc4fc997dda0e26fea1
Gonzalo Genini <gonzalogenini@gmail.com>
2023-10-28 19:09:30 -0300

    agregamos comentarios y una variable

---
+++ js/1-variables.js
@@ -0,0 +1,11 @@
+// esto es un comentario de una sola linea
+
+/*
+esto es un comentario
+de 
+multiples lineas
+*/
+// variable(almacena datos)
+// iniciar una variables (var, let, const) 
+
+let comision = "c74i"
