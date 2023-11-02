40596325d858303d601aba4114e061349287af70
Gonzalo Genini <gonzalogenini@gmail.com>
2023-11-02 01:34:50 -0300

    se vio documento write ventana emergente y como solicitar al usuario ingresar datos

--- js/1-variables.js
+++ js/1-variables.js
@@ -14,3 +14,21 @@ let comision = "c74i"
 console.log(7+8);
 console.log(comision)
 console.log("comision: " + comision)
+
+let numero1 = 10;
+let numero2 = 11;
+// let resultado = null;
+// Suma de numero1 y numero2
+// resultado = numero1 + numero2;
+console.log("El resultado de la suma es: "+ (numero1 + numero2));
+
+// muestra el mensaje en el documento html
+document.write("Hello world <br> soy batman")
+
+// ventanas emergentes 
+// alert("esto es una alerta");
+
+// solicitar un color al usuario 
+let color = prompt("Ingrese su color favorito");
+// sirve tambien para verificar si se guardo el dato en la variable
+console.log(color);
